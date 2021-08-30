3262dac698f0b11d83fcb3a72cdfc86373a9c5a0
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-08-30 20:23:46 +0900

    레이드 스키마 생성

--- schema/raid.js
+++ schema/raid.js
@@ -1,8 +1,45 @@
-const moongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-const raid_schema = new moongoose.Schema({
+const raid_schema = new mongoose.Schema({
 	guildId: {
+		type: [String],
+		required: true,
+	},
+	raidId: {
+		type: Number,
+		required: true,
+	},
+	type: {
+		type: Number,
+		required: true,
+	},
+	boss: {
 		type: String,
 		required: true,
 	},
+	level: {
+		type: String,
+		required: true,
+	},
+	leader: {
+		type: String,
+		required: true,
+	},
+	party1: {
+		type: [String],
+		required: true,
+	},
+	party2: {
+		type: [String],
+	},
+	time: {
+		type: Date,
+		required: true,
+	},
+	detail: {
+		type: String,
+		default: '세부정보가 없습니다',
+	},
 })
+
+module.exports = mongoose.model('raid', raid_schema)
