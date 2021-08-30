1fb69c6a366bc2c280dc3e05f583929058e0ca96
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-08-30 22:57:37 +0900

    Update valtan.js

--- commands/raid/valtan.js
+++ commands/raid/valtan.js
@@ -2,6 +2,7 @@ const error_handler = require('../../tools/error_handler')
 const check_args = require('../../tools/check_args')
 const create_raid = require('../../tools/create_raid')
 const create_embed = require('../../tools/create_embed')
+const insert_msg = require('../../tools/insert_msg')
 
 module.exports = {
 	name: '발탄',
@@ -28,6 +29,12 @@ module.exports = {
 
 			const embed = await create_embed(client, raid_info, 1)
 			let msg = await message.channel.send(embed)
+			await insert_msg(
+				msg.guild.id,
+				msg.channel.id,
+				msg.id,
+				raid_info.raidId
+			)
 			await msg.react('🤚')
 		} catch (e) {
 			// console.log(error_handler(e))
